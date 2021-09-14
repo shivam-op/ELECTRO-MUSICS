3e5d2bea4c97a62cfb261a7fe77f0685cc691eef
darkophacker0987 <82193878+darkophacker0987@users.noreply.github.com>
2021-09-14 17:37:15 +0530

    Create private.py

---
+++ handlers/private.py
@@ -0,0 +1,50 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+from helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+      await message.reply_text(
+        f"""**Hey, I'm {bn} 👀🔥
+🤖Hᴇʏ, ɪ'ᴍ ᴠᴄ ʙᴏᴛ❤️🔥
+I Cᴀɴ Pʟᴀʏ Mᴜsɪᴄ Iɴ Yᴏᴜʀ Gʀᴏᴜᴩ Vᴏɪᴄᴇ Cʜᴀᴛ. 
+Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴩ Aɴᴅ Pʟᴀʏ Mᴜsɪᴄ Fʀᴇᴇʟʏ!
+/help - to get commands
+
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🎶Oᴡɴᴇʀ🎵", url="https://t.me/Itz_me_cyberking")
+                  ],[
+                    InlineKeyboardButton(
+                        "Sᴜᴘᴘᴏʀᴛ🔥", url="https://t.me/ELECTRO_USERBOT_SUPPORT"
+                    ),
+                    InlineKeyboardButton(
+                        "Uᴘᴅᴀᴛᴇ🛠", url="https://t.me/ELECTRO_USERBOT"
+                    )    
+                ],[ 
+                    InlineKeyboardButton(
+                        "➕Aᴅᴅ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ➕", url="https://t.me/ELECTRO_MUSIC_BOT?startgroup=true"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""**Yes iᴍ online ✔**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔊Uᴩᴅᴀᴛᴇs", url="https://t.me/ELECTRO_USERBOT")
+                ]
+            ]
+        )
+   )
